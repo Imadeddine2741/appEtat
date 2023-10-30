f279b3f1e3699387f3d8d58fdf1d3a27baed0713
imad <imadeddine.rahmani@gmail.com>
2023-10-30 10:26:35 +0100

    add class Main

--- src/test/java/com/ism/appEtat/AppEtatApplicationTests.java
+++ src/test/java/com/ism/appEtat/AppEtatApplicationTests.java
@@ -1,13 +1,10 @@
 package com.ism.appEtat;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AppEtatApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
