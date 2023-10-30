6597d2e39fa8433436189d4e265b845c85bc119f
imad <imadeddine.rahmani@gmail.com>
2023-10-30 10:16:39 +0100

    first commit

---
+++ src/main/java/com/ism/appEtat/AppEtatApplication.java
@@ -0,0 +1,13 @@
+package com.ism.appEtat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEtatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEtatApplication.class, args);
+	}
+
+}
